089b09e28cfd69d664d3b2a3f6f5b85ea82e4fbd
Valdir Silva <valdir.ti@gmail.com>
2022-12-12 21:54:04 -0300

    feat: create initial route ro tests

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import { authRouter } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({ message: "Initial Route" });
+});
 
 app.use(authRouter);
 
