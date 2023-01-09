1b7700690d8d7a49904065540de607ba117bb190
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-01-09 17:43:39 -0300

    chore: add and configure commit lint, eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express, {Request, Response} from "express";
-import { authRouter } from "./routes";
+import express, { Request, Response } from "express"
+import { authRouter } from "./routes"
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 app.get("/", (req: Request, res: Response) => {
-    res.status(200).json({ message: "Intratec Api initial route" });
-});
+  res.status(200).json({ message: "Intratec Api initial route" })
+})
 
-app.use("/auth", authRouter);
+app.use("/auth", authRouter)
 
-export { app };
+export { app }
