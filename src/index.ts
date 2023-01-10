1abaa2458410254362f6772ec35ba6aa7902751e
Valdir Silva <valdir.ti@gmail.com>
2023-01-09 22:16:40 -0300

    chore: add jest

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,32 @@
-import dotenv from "dotenv";
-import { app } from "./app";
-import mongoose from "mongoose";
+import dotenv from "dotenv"
+import { app } from "./app"
+import mongoose from "mongoose"
 
-dotenv.config();
+dotenv.config()
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3333
 
 const connect = async () => {
+  console.log(process.env.MONGO_PASSWORD)
   try {
-    const mongo_url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.72clvyp.mongodb.net/?retryWrites=true&w=majority`;
-    mongoose.set("strictQuery", false);
-    await mongoose.connect(mongo_url || "");
-    console.log("Connected to mongoDB");
+    const mongo_url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.72clvyp.mongodb.net/?retryWrites=true&w=majority`
+    mongoose.set("strictQuery", false)
+    await mongoose.connect(mongo_url || "")
+    console.log("Connected to mongoDB")
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 mongoose.connection.on("disconnected", () => {
-  console.log("mongoDB disconnected");
-});
+  console.log("mongoDB disconnected")
+})
 
 mongoose.connection.on("connected", () => {
-  console.log("mongoDB connected");
-});
+  console.log("mongoDB connected")
+})
 
 app.listen(port, () => {
-  connect();
-  console.log("app listening port: " + port);
-});
+  connect()
+  console.log("app listening port: " + port)
+})
---
+++ src/useCases/Users/CreateUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,5 @@
+describe("Create user use case", () => {
+  test("should return 4", () => {
+    expect(1 + 2).toBe(3)
+  })
+})
